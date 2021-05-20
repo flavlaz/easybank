85cfb3176a0226ad1c58009c555d41f31133c585
flavlaz <flaviu_lazar@gmail.com>
2021-05-20 11:32:11 +0300

    adding menu  overlay on mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log('salut');
+//hamburger menu functionality
+const menuBtn = document.getElementById('menu-btn');
+const header = document.getElementById('header');
+const overlay = document.getElementById('overlay');
+
+menuBtn.addEventListener('click', ()=> {
+    header.classList.toggle('open');
+    if(header.classList.contains('open')){
+        overlay.classList.remove('over-out')
+        overlay.classList.add('over-in');
+    } else {
+        overlay.classList.remove('over-in');
+        overlay.classList.add('over-out');
+    }
+})
